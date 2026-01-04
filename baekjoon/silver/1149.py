470d21b69b483fa21495472d896a886c7cb6171e
danmoo6837 <dlehddbs6837@naver.com>
2026-01-04 14:19:48 +0900

    feat: RGB거리

---
+++ baekjoon/silver/1149.py
@@ -0,0 +1,17 @@
+from sys import stdin
+
+input = stdin.readline
+
+if __name__ == "__main__":
+    n = int(input())
+    arr = [None]*n
+    
+    for i in range(n):
+        arr[i] = list(map(int, input().split()))
+
+    for i in range(1, n):
+        arr[i][0] = min(arr[i-1][1], arr[i-1][2]) + arr[i][0]
+        arr[i][1] = min(arr[i-1][0], arr[i-1][2]) + arr[i][1]
+        arr[i][2] = min(arr[i-1][0], arr[i-1][1]) + arr[i][2]
+
+    print(min(arr[n-1][0], arr[n-1][1], arr[n-1][2]))
